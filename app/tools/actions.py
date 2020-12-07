250e5aacaeb00237bf32874def55587dbae59abf
brightrock <ricekent@163.com>
2020-12-07 19:27:56 +0800

    id transfer

--- app/tools/actions.py
+++ app/tools/actions.py
@@ -24,14 +24,14 @@ from scipy.spatial.distance import pdist
 from itertools import groupby
 from operator import itemgetter
 
-UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'download')
+UPLOAD_FOLDER = os.path.join(basedir, "app", "static", "download")
 vcf_seq_script = os.path.join(Config.SCRIPT_PATH, Config.VCF_SEQ)
 vcf_ann_script = os.path.join(Config.SCRIPT_PATH, Config.VCF_ANN)
 vcf_pca_script = os.path.join(Config.SCRIPT_PATH, Config.VCF_PCA)
 
-VAR_DENSITY_PATH = os.path.join(UPLOAD_FOLDER, 'var_density')
+VAR_DENSITY_PATH = os.path.join(UPLOAD_FOLDER, "var_density")
 
-GENE_PATTERN = re.compile('TraesCS(\w+)1G(\w+)')
+GENE_PATTERN = re.compile("TraesCS(\w+)1G(\w+)")
 
 
 def transfer_id(gene_id):
@@ -40,26 +40,31 @@ def transfer_id(gene_id):
     """
     if GENE_PATTERN.match(gene_id):
         id_1, id_2 = GENE_PATTERN.match(gene_id).groups()
-        return 'TraesCS{id_1}2G{id_2}'.format(**locals())
+        return "TraesCS{id_1}2G{id_2}".format(**locals())
     else:
         return gene_id
 
 
 def wildcard_gene(genename):
-    return '%'.join([genename[:10], genename[11:]])
+    return "%".join([genename[:10], genename[11:]])
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ('xlsx', 'csv', 'txt')
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in (
+        "xlsx",
+        "csv",
+        "txt",
+    )
 
 
 def query_gene_by_pos(chrom, start, end, **kargs):
     if chrom and start and end:
-        gene_bed_df = pd.read_csv(Config.GENE_POS,
-                                  sep='\t',
-                                  header=None,
-                                  names=['chrom', 'start', 'end', 'gene'])
+        gene_bed_df = pd.read_csv(
+            Config.GENE_POS,
+            sep="\t",
+            header=None,
+            names=["chrom", "start", "end", "gene"],
+        )
         filter1 = gene_bed_df.chrom == chrom
         filter2 = gene_bed_df.start >= int(start)
         filter3 = gene_bed_df.end <= int(end)
@@ -69,9 +74,9 @@ def query_gene_by_pos(chrom, start, end, **kargs):
 
 
 def batch_query_gene(gene_list, max_input=1000):
-    '''
+    """
     get a gene string by search locus database
-    '''
+    """
 
     if len(gene_list) > max_input:
         return []
@@ -83,98 +88,108 @@ def batch_query_gene(gene_list, max_input=1000):
     cmd = """select l.*, f.Description, f.Pfam_Description,
                      f.Interpro_Description, f.GO_Description from locus l left join func f
                      on l.GENE_ID=f.GENE_ID where l.GENE_ID in {0};
-          """.format(_search)
+          """.format(
+        _search
+    )
     result = db.execute(cmd)
     if result:
-        result = [(each[1], ) + each[5:] for each in result]
+        result = [(each[1],) + each[5:] for each in result]
         df1 = pd.DataFrame(result)
         df2 = pd.DataFrame(gene_list)
-        df3 = pd.merge(df2, df1, how='left').fillna("")
+        df3 = pd.merge(df2, df1, how="left").fillna("")
         return [list(df3.iloc[i, :]) for i in range(len(df3))]
     return []
 
 
 def fetch_gene_seq(geneId, feature):
+    geneId = transfer_id(geneId)
     seq_res = GeneSeq.query.filter_by(gene_id=geneId, feature=feature).all()
     seq_list = [item.as_dict() for item in seq_res]
     tr_seq_dict = {}
     for tr_id, seq_data in groupby(seq_list, key=itemgetter("transcript_id")):
-        tr_seq = [each['seq'] for each in seq_data]
-        tr_seq_dict[tr_id] = '\n'.join(tr_seq)
+        tr_seq = [each["seq"] for each in seq_data]
+        tr_seq_dict[tr_id] = "\n".join(tr_seq)
     return tr_seq_dict
 
 
 def get_locus_result(genename, blast_results):
-    cds_seq_dict = blast_results.get('cds_seq', 'NA')
-    pro_seq_dict = blast_results.get('pro_seq', 'NA')
+    cds_seq_dict = blast_results.get("cds_seq", "NA")
+    pro_seq_dict = blast_results.get("pro_seq", "NA")
 
     db = DB()
     locus_result = {}
     cmd = """select l.*, f.Description, f.Pfam_Description,
              f.Interpro_Description, f.GO_Description from locus l left join func f
              on l.GENE_ID=f.GENE_ID where l.GENE_ID='{0}';
-          """.format(genename)
+          """.format(
+        genename
+    )
     result = db.execute(cmd, get_all=False)
     if result:
-        locus_result['orthologous_gene'] = {}
+        locus_result["orthologous_gene"] = {}
         ortho_header = [
-            'Arabidopsis_thaliana', 'Hordeum_vulgare', 'Oryza_sativa',
-            'Triticum_aestivum', 'Zea_mays'
+            "Arabidopsis_thaliana",
+            "Hordeum_vulgare",
+            "Oryza_sativa",
+            "Triticum_aestivum",
+            "Zea_mays",
         ]
-        locus_result['orthologous_gene']['header'] = ortho_header
-        locus_result['orthologous_gene']['body'] = []
+        locus_result["orthologous_gene"]["header"] = ortho_header
+        locus_result["orthologous_gene"]["body"] = []
         cmd = "select l.GENE_ID, o.* from locus l left join ortho o on l.GENE_ID=o.GENE_ID where l.GENE_ID='{0}';".format(
-            genename)
+            genename
+        )
         ortho_result = db.execute(cmd, get_all=False)
         if ortho_result:
             ortho_result_list = ortho_result[3:]
             ortho_result_list = [
-                addOuterLink(each) for each in ortho_result_list
-                if each is not None
+                addOuterLink(each) for each in ortho_result_list if each is not None
             ]
-            locus_result['orthologous_gene']['body'] = ortho_result_list
+            locus_result["orthologous_gene"]["body"] = ortho_result_list
         gene_id, chr, pos_start, pos_end = result[1:5]
         description, pfam_desc, interpro_desc, go_desc = result[5:]
-        locus_result['gene_identification'] = {
-            'Gene Product Name': description,
-            'Locus Name': genename
+        locus_result["gene_identification"] = {
+            "Gene Product Name": description,
+            "Locus Name": genename,
         }
-        locus_result['gene_attributes'] = {
-            'Chromosome': chr,
-            "Gene Postion": '{start} - {end}'.format(start=pos_start,
-                                                     end=pos_end)
+        locus_result["gene_attributes"] = {
+            "Chromosome": chr,
+            "Gene Postion": "{start} - {end}".format(start=pos_start, end=pos_end),
         }
         header = [
-            'Description', 'Pfam_Description', 'Interpro_Description',
-            'GO_Description'
+            "Description",
+            "Pfam_Description",
+            "Interpro_Description",
+            "GO_Description",
         ]
-        locus_result['gene_annotation'] = {}
-        locus_result['gene_annotation']['header'] = header
-        locus_result['gene_annotation']['body'] = [
-            description, pfam_desc, interpro_desc, go_desc
+        locus_result["gene_annotation"] = {}
+        locus_result["gene_annotation"]["header"] = header
+        locus_result["gene_annotation"]["body"] = [
+            description,
+            pfam_desc,
+            interpro_desc,
+            go_desc,
         ]
         # match 01G and 02G TraesCS1A02G000100
-        #result = db.execute("select * from tissue_expression where Gene_id='{0}'".format(genename))
+        # result = db.execute("select * from tissue_expression where Gene_id='{0}'".format(genename))
         result = db.execute(
             "select * from tissue_expression where Gene_id like '{0}'".format(
-                wildcard_gene(genename)))
+                wildcard_gene(genename)
+            )
+        )
         if result:
             row = [np.log2(float(each) + 1) for each in result[0][2:]]
         else:
             row = []
-        locus_result['tissue_expression'] = row
-        locus_result['gene_cds_seq'] = cds_seq_dict
-        locus_result['gene_pro_seq'] = pro_seq_dict
+        locus_result["tissue_expression"] = row
+        locus_result["gene_cds_seq"] = cds_seq_dict
+        locus_result["gene_pro_seq"] = pro_seq_dict
 
         geneExp = GeneExpression.findbygene(transfer_id(genename))
-        locus_result['dev_expression_stage'] = [
-            each.tissue for each in geneExp
-        ]
-        locus_result['dev_expression_tpm'] = [
-            np.log2(each.tpm + 1) for each in geneExp
-        ]
-        all_tpm_list = row + locus_result['dev_expression_tpm']
-        locus_result['max_tpm'] = np.ceil(max(all_tpm_list))
+        locus_result["dev_expression_stage"] = [each.tissue for each in geneExp]
+        locus_result["dev_expression_tpm"] = [np.log2(each.tpm + 1) for each in geneExp]
+        all_tpm_list = row + locus_result["dev_expression_tpm"]
+        locus_result["max_tpm"] = np.ceil(max(all_tpm_list))
     return locus_result
 
 
@@ -185,30 +200,32 @@ def fetch_sequence(table, chr, start_pos, end_pos):
     --sample_name {sample}  \
     --chrom {chr} \
     --start_pos {start_pos} \
-    --end_pos {end_pos}".format(script=vcf_seq_script,
-                                data_path=Config.VCF_SAMPLE_PATH,
-                                table=table,
-                                sample=table.split(".")[1],
-                                chr=chr,
-                                start_pos=start_pos,
-                                end_pos=end_pos)
+    --end_pos {end_pos}".format(
+        script=vcf_seq_script,
+        data_path=Config.VCF_SAMPLE_PATH,
+        table=table,
+        sample=table.split(".")[1],
+        chr=chr,
+        start_pos=start_pos,
+        end_pos=end_pos,
+    )
     print(cmd)
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     p.wait()
     result = p.stdout.readlines()
-    result = ''.join([
-        str(each, encoding="utf-8").replace('\n', '<br/>') for each in result
-    ])
+    result = "".join(
+        [str(each, encoding="utf-8").replace("\n", "<br/>") for each in result]
+    )
     return result
 
 
 def _pandas_read(filename, header):
-    suffix = filename.rsplit('.', 1)[1].lower()
-    if suffix == 'csv':
+    suffix = filename.rsplit(".", 1)[1].lower()
+    if suffix == "csv":
         df = pd.read_csv(filename, header=header)
-    elif suffix == 'xlsx':
+    elif suffix == "xlsx":
         df = pd.read_excel(filename, header=header)
-    elif suffix == 'txt':
+    elif suffix == "txt":
         df = pd.read_table(filename, header=header)
     else:
         return pd.DataFrame([])
@@ -217,16 +234,13 @@ def _pandas_read(filename, header):
 
 def run_pca(filename, sample_group):
     pca = PCA(n_components=2)
-    df = _pandas_read(os.path.join(UPLOAD_FOLDER, 'pca', filename), header=0)
-    mat = [
-        list(each)
-        for each in pca.fit_transform(np.log2(df.iloc[:, 1:] + 1).T)
-    ]
+    df = _pandas_read(os.path.join(UPLOAD_FOLDER, "pca", filename), header=0)
+    mat = [list(each) for each in pca.fit_transform(np.log2(df.iloc[:, 1:] + 1).T)]
 
     if sample_group:
-        sample_df = _pandas_read(os.path.join(UPLOAD_FOLDER, 'group',
-                                              sample_group),
-                                 header=None)
+        sample_df = _pandas_read(
+            os.path.join(UPLOAD_FOLDER, "group", sample_group), header=None
+        )
         group = list(sample_df.iloc[:, 0])
         sample = list(sample_df.iloc[:, 1])
 
@@ -242,7 +256,7 @@ def run_pca(filename, sample_group):
 
 
 def run_vcf(filepath):
-    tmp_pca_path = os.path.join(UPLOAD_FOLDER, 'pca', 'tmp.pca.txt')
+    tmp_pca_path = os.path.join(UPLOAD_FOLDER, "pca", "tmp.pca.txt")
     command = "sh {0} {1} {2}".format(vcf_pca_script, filepath, tmp_pca_path)
     p = subprocess.Popen(command, shell=True)
     p.wait()
@@ -253,7 +267,7 @@ def run_vcf(filepath):
     for i in range(len(df)):
         mat.append(list(df.iloc[i, 1:]))
 
-    group = list(df['ID'])
+    group = list(df["ID"])
     sample = copy.copy(group)
 
     for i in range(len(mat)):
@@ -264,75 +278,79 @@ def run_vcf(filepath):
 
 
 def run_annotation(vcf_file, annotation_database):
-    annotation_prefix = '-'.join(
-        [str(int(time.time())),
-         vcf_file.split('.vcf.gz')[0]])
+    annotation_prefix = "-".join([str(int(time.time())), vcf_file.split(".vcf.gz")[0]])
     cmd = "{script}  {vcf_file} {annotation_database} {prefix}".format(
         script=vcf_ann_script,
-        vcf_file=os.path.join(UPLOAD_FOLDER, 'vcf_ann', vcf_file),
+        vcf_file=os.path.join(UPLOAD_FOLDER, "vcf_ann", vcf_file),
         annotation_database=annotation_database,
-        prefix=os.path.join(UPLOAD_FOLDER, 'vcf_ann', annotation_prefix))
+        prefix=os.path.join(UPLOAD_FOLDER, "vcf_ann", annotation_prefix),
+    )
     processor.shRun(cmd)
-    processor.Run("zip {zipfile} {files}".format(
-        zipfile=os.path.join(UPLOAD_FOLDER, 'vcf_ann',
-                             annotation_prefix + '.zip'),
-        files=os.path.join(UPLOAD_FOLDER, 'vcf_ann', annotation_prefix) +
-        '.ann.vcf.*'))
-    return annotation_prefix + '.zip'
+    processor.Run(
+        "zip {zipfile} {files}".format(
+            zipfile=os.path.join(UPLOAD_FOLDER, "vcf_ann", annotation_prefix + ".zip"),
+            files=os.path.join(UPLOAD_FOLDER, "vcf_ann", annotation_prefix)
+            + ".ann.vcf.*",
+        )
+    )
+    return annotation_prefix + ".zip"
 
 
 @celery.task
 def async_run_annotation(vcf_file, annotation_database):
-    annotation_prefix = '-'.join(
-        [str(int(time.time())),
-         vcf_file.split('.vcf.gz')[0]])
+    annotation_prefix = "-".join([str(int(time.time())), vcf_file.split(".vcf.gz")[0]])
     cmd = "{script}  {vcf_file} {annotation_database} {prefix}".format(
         script=vcf_ann_script,
-        vcf_file=os.path.join(UPLOAD_FOLDER, 'vcf_ann', vcf_file),
+        vcf_file=os.path.join(UPLOAD_FOLDER, "vcf_ann", vcf_file),
         annotation_database=annotation_database,
-        prefix=os.path.join(UPLOAD_FOLDER, 'vcf_ann', annotation_prefix))
+        prefix=os.path.join(UPLOAD_FOLDER, "vcf_ann", annotation_prefix),
+    )
     processor.shRun(cmd)
-    processor.Run("zip {zipfile} {files}".format(
-        zipfile=os.path.join(UPLOAD_FOLDER, 'vcf_ann',
-                             annotation_prefix + '.zip'),
-        files=os.path.join(UPLOAD_FOLDER, 'vcf_ann', annotation_prefix) +
-        '.ann.vcf.*'))
-    result = annotation_prefix + '.zip'
-    return {'task': 'vcf_ann', 'result': result}
+    processor.Run(
+        "zip {zipfile} {files}".format(
+            zipfile=os.path.join(UPLOAD_FOLDER, "vcf_ann", annotation_prefix + ".zip"),
+            files=os.path.join(UPLOAD_FOLDER, "vcf_ann", annotation_prefix)
+            + ".ann.vcf.*",
+        )
+    )
+    result = annotation_prefix + ".zip"
+    return {"task": "vcf_ann", "result": result}
 
 
 def fetch_lnc_pcg_pairs(options):
     print(options)
-    genes = [each for each in options['genes'] if each]
+    genes = [each for each in options["genes"] if each]
     if genes:
-        mrna_res = RnaNeighbor.query.filter(
-            RnaNeighbor.mRNA_gene.in_(genes)).all()
-        lnc_res = RnaNeighbor.query.filter(
-            RnaNeighbor.lncRNA_gene.in_(genes)).all()
+        mrna_res = RnaNeighbor.query.filter(RnaNeighbor.mRNA_gene.in_(genes)).all()
+        lnc_res = RnaNeighbor.query.filter(RnaNeighbor.lncRNA_gene.in_(genes)).all()
     else:
-        chrom = options['chrom']
-        start = options['start']
-        end = options['end']
-        mrna_res = RnaNeighbor.query.filter(RnaNeighbor.chrom == chrom,
-                                            RnaNeighbor.mRNA_start >= start,
-                                            RnaNeighbor.mRNA_end <= end).all()
+        chrom = options["chrom"]
+        start = options["start"]
+        end = options["end"]
+        mrna_res = RnaNeighbor.query.filter(
+            RnaNeighbor.chrom == chrom,
+            RnaNeighbor.mRNA_start >= start,
+            RnaNeighbor.mRNA_end <= end,
+        ).all()
         lnc_res = RnaNeighbor.query.filter(
-            RnaNeighbor.chrom == chrom, RnaNeighbor.lncRNA_start >= start,
-            RnaNeighbor.lncRNA_end <= end).all()
+            RnaNeighbor.chrom == chrom,
+            RnaNeighbor.lncRNA_start >= start,
+            RnaNeighbor.lncRNA_end <= end,
+        ).all()
     return mrna_res + lnc_res
 
 
 def fetch_pcg_exp(geneList: List[str], sampleList: List[str]):
     query_out = GeneExpression.query.filter(
-        GeneExpression.gene_id.in_(geneList),
-        GeneExpression.tissue.in_(sampleList)).all()
+        GeneExpression.gene_id.in_(geneList), GeneExpression.tissue.in_(sampleList)
+    ).all()
     return [item.as_dict() for item in query_out]
 
 
 def fetch_lnc_exp(geneList: List[str], sampleList: List[str]):
     query_out = LncExpression.query.filter(
-        LncExpression.gene_id.in_(geneList),
-        LncExpression.tissue.in_(sampleList)).all()
+        LncExpression.gene_id.in_(geneList), LncExpression.tissue.in_(sampleList)
+    ).all()
     return [item.as_dict() for item in query_out]
 
 
@@ -345,8 +363,8 @@ def fetch_exp_item(genes, expItems):
     for gene in genes:
         gene_exp_list = []
         for exp_i in expItems:
-            if exp_i['gene_id'] == gene:
-                gene_exp_list.append(normalized_tpm(exp_i['tpm']))
+            if exp_i["gene_id"] == gene:
+                gene_exp_list.append(normalized_tpm(exp_i["tpm"]))
         expList.append(gene_exp_list)
     return expList
 
@@ -355,18 +373,20 @@ def pearson_cor(mrna, lncrna, mrnaExp, lncExp, mrna_lnc_pair):
     exp_product = list(product(mrnaExp, lncExp))
     results = []
     for n, gene_pair in enumerate(product(mrna, lncrna)):
-        gene_pair_str = '-'.join(gene_pair)
+        gene_pair_str = "-".join(gene_pair)
         if gene_pair_str in mrna_lnc_pair:
             pcc, pval = stats.pearsonr(exp_product[n][0], exp_product[n][1])
             if pcc is np.nan:
                 pcc = 0
                 pval = 1
-            results.append({
-                "mRNA": gene_pair[0],
-                "lncRNA": gene_pair[1],
-                "pcc": round(pcc, 3),
-                "p-value": pval
-            })
+            results.append(
+                {
+                    "mRNA": gene_pair[0],
+                    "lncRNA": gene_pair[1],
+                    "pcc": round(pcc, 3),
+                    "p-value": pval,
+                }
+            )
     return results
 
 
